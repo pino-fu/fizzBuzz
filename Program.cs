8affcb549d006f67bd5eaf26d084bee7080bea7a
Ben Hoberg <benjhoberg@gmail.com>
2023-03-03 14:38:13 -0600

    fizzBuzz challenge complete

--- Program.cs
+++ Program.cs
@@ -4,9 +4,28 @@ namespace fizzBuzz
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+             }
+            }
         }
     }
 }
